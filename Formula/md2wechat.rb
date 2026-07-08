1b5f7f642436cf2fa309eccd1db1c0f3dc91db91
geekjourneyx <zlisreallyli@outlook.com>
2026-07-08 15:19:52 +0000

    md2wechat v2.10.1

--- Formula/md2wechat.rb
+++ Formula/md2wechat.rb
@@ -2,27 +2,27 @@ class Md2wechat < Formula
   desc "Convert Markdown to WeChat Official Account HTML"
   homepage "https://github.com/geekjourneyx/md2wechat-skill"
   license "proprietary"
-  version "2.10.0"
+  version "2.10.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/geekjourneyx/md2wechat-skill/releases/download/v2.10.0/md2wechat_Darwin_arm64.tar.gz"
-      sha256 "83dd50fd9cab2121daaac9647b4828a4393fe86064d8eaed4b24732954152c26"
+      url "https://github.com/geekjourneyx/md2wechat-skill/releases/download/v2.10.1/md2wechat_Darwin_arm64.tar.gz"
+      sha256 "d5cc9c1ea64ea7110fb3d0d0def493d6da2ea38be0b6fbbf6e919e3695106e17"
     end
     on_intel do
-      url "https://github.com/geekjourneyx/md2wechat-skill/releases/download/v2.10.0/md2wechat_Darwin_x86_64.tar.gz"
-      sha256 "a608b98ef7718ed4e618eadaeed83f862663647e6688a87f61ec314ae9f5eb48"
+      url "https://github.com/geekjourneyx/md2wechat-skill/releases/download/v2.10.1/md2wechat_Darwin_x86_64.tar.gz"
+      sha256 "55ae4d810052657fdf8689ff00de71289923522bb374deac1d59a86e65511267"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/geekjourneyx/md2wechat-skill/releases/download/v2.10.0/md2wechat_Linux_arm64.tar.gz"
-      sha256 "d01d11708450393f27f2244701f6b081f91a03b3d45f2a0af2e8640d26fd3b3c"
+      url "https://github.com/geekjourneyx/md2wechat-skill/releases/download/v2.10.1/md2wechat_Linux_arm64.tar.gz"
+      sha256 "41f42155a3ee3c50271e1f1bad653b97a2a7ea81d13013956e9ab4b26c4fac79"
     end
     on_intel do
-      url "https://github.com/geekjourneyx/md2wechat-skill/releases/download/v2.10.0/md2wechat_Linux_x86_64.tar.gz"
-      sha256 "7dc43993699f84e53bd49bfc26e8c15038a8b627ee52696f80a9a55ea53c4c56"
+      url "https://github.com/geekjourneyx/md2wechat-skill/releases/download/v2.10.1/md2wechat_Linux_x86_64.tar.gz"
+      sha256 "42e6d2f69df2484ebe14e755de04f627567d328bacaafa55941dd7288e3acc59"
     end
   end
 
